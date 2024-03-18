adcdb966ae5465cd4017dba198f75f8edcadc7c2
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-03-18 16:09:51 +0000

    add favicon

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const key = 'd53785c1e33da9456359647e94822b7f';
 
 async function search() {
     const phrase = document.querySelector('input[type="text"]').value;
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${key}`)
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${key}`)
     const data = await response.json()
     const ul = document.querySelector('form ul');
     ul.innerHTML = '';
